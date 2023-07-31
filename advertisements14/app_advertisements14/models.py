9ad9955e8954d8eaf2bf83427342e41c7cc98ac1
Spectrum <vadik8c@gmail.com>
2023-07-31 18:46:23 +0300

    homework #7

--- advertisements14/app_advertisements14/models.py
+++ advertisements14/app_advertisements14/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Advertisements14(models.Model):
+    title = models.CharField('Заголовок', max_length=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Торг', help_text='Отметьте, если возможен торг')
+    connection = models.BooleanField('Связь', help_text='Отметьте, если возможна связь с продавцом', default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Advertisements14(id={self.id}, title={self.title}, price={self.price}'
+    class Meta:
+        db_table = 'advertisements'
