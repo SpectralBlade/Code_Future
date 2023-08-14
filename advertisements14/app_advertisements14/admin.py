dae12a22b28d3bd79fc5b34a1aa92279f08d0af9
Spectrum <vadik8c@gmail.com>
2023-08-14 18:49:24 +0300

    homework 14.08

--- advertisements14/app_advertisements14/admin.py
+++ advertisements14/app_advertisements14/admin.py
@@ -2,25 +2,25 @@ from django.contrib import admin
 from .models import Advertisements14
 
 class Advertisements14Admin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description', 'price', 'auction', 'connection', 'created_date', 'updated_date']
+    list_display = ['id', 'title', 'description', 'price', 'auction', 'connection', 'created_date', 'updated_date', 'get_html_image']
     list_filter = ['auction', 'created_time']
     actions = ['make_auction_as_false', 'make_auction_as_true']
     fieldsets = (
         ('Общее', {
-            'fields':('title', 'description', 'connection')
+            'fields': ('title', 'description', 'connection', 'image'),
         }),
         ('Финансы', {
-            'fields':('price', 'auction'),
-            'classes':['collapse']
+            'fields': ('price', 'auction'),
+            'classes': ['collapse']
         })
     )
 
     @admin.action(description='Убрать возможность торга')
     def make_auction_as_false(self, request, queryset):
-        queryset.update(auction='False')
+        queryset.update(auction=False)
 
     @admin.action(description='Добавить возможность торга')
     def make_auction_as_true(self, request, queryset):
-        queryset.update(auction='True')
+        queryset.update(auction=True)
 
 admin.site.register(Advertisements14, Advertisements14Admin)
--- advertisements14/app_advertisements14/models.py
+++ advertisements14/app_advertisements14/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Advertisements14(models.Model):
-    title = models.CharField('Заголовок', max_length=128)
-    description = models.TextField('Описание')
-    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
-    auction = models.BooleanField('Торг', help_text='Отметьте, если возможен торг')
-    connection = models.BooleanField('Связь', help_text='Отметьте, если возможна связь с продавцом', default=True)
+    title = models.CharField('заголовок', max_length=128)
+    description = models.TextField('описание')
+    price = models.DecimalField('цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('торг', help_text='Отметьте, если возможен торг')
+    connection = models.BooleanField('связь', help_text='Отметьте, возможна ли связь с продавцом', default=True)
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, verbose_name='пользователь', on_delete=models.CASCADE, db_constraint=False)
+    image = models.ImageField('изображение', upload_to="advertisements/")
 
-    @admin.display(description='Дата создания')
+    @admin.display(description='дата создания')
     def created_date(self):
         from django.utils import timezone
         if self.created_time.date() == timezone.now().date():
@@ -21,7 +26,7 @@ class Advertisements14(models.Model):
             )
         return self.created_time.strftime("%d.%m.%Y в %H:%M:%S")
 
-    @admin.display(description='Дата изменения')
+    @admin.display(description='дата последнего обновления')
     def updated_date(self):
         from django.utils import timezone
         if self.updated_time.date() == timezone.now().date():
@@ -31,7 +36,16 @@ class Advertisements14(models.Model):
             )
         return self.updated_time.strftime("%d.%m.%Y в %H:%M:%S")
 
+    @admin.display(description='фото')
+    def get_html_image(self):
+        if self.image:
+            return format_html(
+                '<img src="{url}" style="max-width: 80px; max-height: 80px;">', url = self.image.url
+            )
+
+
     def __str__(self):
-        return f'Advertisements14(id={self.id}, title={self.title}, price={self.price}'
+        return f"Advertisements14(id={self.id}, title={self.title}, price={self.price})"
+
     class Meta:
-        db_table = 'advertisements'
+        db_table = "advertisements"
